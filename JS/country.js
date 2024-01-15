4b6988f11ff46f040973a2a6836ed0a00b0dbb42
AntonioAzevedoC <148168409+AntonioAzevedoC@users.noreply.github.com>
2024-01-14 23:33:11 -0300

    Add files via upload

--- JS/country.js
+++ JS/country.js
@@ -1,9 +1,12 @@
 "use strict";
 
 // Variables
+const content = document.querySelector(".__content");
 const leftSide = document.querySelector(".__left-side");
 const center = document.querySelector(".__center");
 const rightSide = document.querySelector(".__right-side");
+const errorContainer = document.querySelector(".__error-display");
+const errorText = document.querySelector(".__error-text");
 const neighboursContainer = document.querySelector(".__neighbours");
 const countrySearch = document.querySelector(".__search");
 const formSearch = document.querySelector(".__form-search");
@@ -20,9 +23,9 @@ const countryDemo = document.querySelector("#__demo");
 const countryCurr = document.querySelector("#__curr");
 const countryWiki = document.querySelector(".__wiki-link");
 
-let neighbours;
 let population;
 let txt;
+let neighbours;
 
 // Render value in array
 const renderArr = function (arr) {
@@ -33,27 +36,38 @@ const renderArr = function (arr) {
   return txt;
 };
 
+// return population
+const returnPop = function (pop, qtt, t = "million") {
+  population = (+pop / qtt).toFixed(2) + ` ${t}`;
+  return population;
+};
+
 // Render country population
 const renderPop = function (pop) {
-  if (pop > 1000000) {
-    population = (+pop / 1000000).toFixed(2) + ` million`;
-    return population;
+  if (pop > 1000000000) {
+    return returnPop(pop, 1000000000, "billion");
+  } else if (pop > 1000000) {
+    return returnPop(pop, 1000000);
   }
   return pop;
 };
 
 // Rendering neighbouring countries
 const getNeighbours = function (arr) {
-  neighbours = Array.from(document.querySelectorAll(".__neighbour-country"));
+  neighbours = Array.from(document.querySelectorAll(".__country-card"));
   neighbours.forEach((el) => {
     el.remove();
   });
 
   arr.forEach((el) => {
     requestCountry(el, "alpha").then((response) => {
-      const name = response.name;
       const html = `
-        <div class=" __neighbour-country" id="__${name}">${name}</div>
+      <div class="card __country-card" country="${response.alpha3Code}">
+        <div class="row">
+          <span class="col-md-6 col-sm-12 mt-3 mb-3 ml-2 text-center __neighbour-country" id="__${response.nativeName}" >${response.name}</span>
+          <img class="d-none d-md-block col-4 p-0 m-1 img-fluid __neighbouring-country-flag" src="${response.flag}" alt="Country flag"></img>
+        </div>
+      </div>
       `;
       neighboursContainer.insertAdjacentHTML("beforeend", html);
     });
@@ -62,8 +76,6 @@ const getNeighbours = function (arr) {
 
 // Rendering country
 const renderCountry = function (country) {
-  console.log(country);
-
   countryName.textContent = country.name;
   countryNativeName.textContent = country.nativeName;
   countryFlag.src = country.flag;
@@ -80,11 +92,15 @@ const renderCountry = function (country) {
   countryWiki.textContent = `${country.name} Wikipedia Article`;
   countryWiki.href = `https://pt.wikipedia.org/wiki/${country.name}`;
   getNeighbours(country.borders);
+  content.classList.remove("d-none");
+  errorContainer.classList.add("d-none");
 };
 
 // Rendering search errors
 const renderError = function (msg) {
-  alert(msg);
+  content.classList.add("d-none");
+  errorText.textContent = msg;
+  errorContainer.classList.remove("d-none");
 };
 
 // handling country request
@@ -102,9 +118,9 @@ const requestCountry = function (
   });
 };
 
-// Getting cpuntry data after country is requested
-const getCountryData = function (country, code = "name") {
-  requestCountry(country, code, "Country not found")
+// Getting country data after country is requested
+const getCountryData = function (country) {
+  requestCountry(country, "name", "Country not found")
     .then(([data]) => {
       renderCountry(data);
     })
@@ -114,7 +130,36 @@ const getCountryData = function (country, code = "name") {
     });
 };
 
+// Getting country data after country is requested
+const getNeighbourCountryData = function (country) {
+  requestCountry(country, "alpha", "Country not found")
+    .then((data) => {
+      renderCountry(data);
+    })
+    .catch((err) => {
+      console.error(`${err}`);
+      renderError(`Something went wrong:  ${err.message}.`);
+    });
+};
+
+// Handling form submit
 formSearch.addEventListener("submit", function (e) {
   e.preventDefault();
+  if (countrySearch.value === "") return;
   getCountryData(countrySearch.value);
+  countrySearch.value = "";
+
+  countrySearch.disabled = true;
+  setTimeout(() => {
+    countrySearch.disabled = false;
+  }, 1500);
+});
+
+// Handling neighbouring countries click
+neighboursContainer.addEventListener("click", function (e) {
+  const click = e.target.closest(".__country-card");
+
+  if (!click) return;
+
+  getNeighbourCountryData([click.getAttribute("country")]);
 });
